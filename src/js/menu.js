fdf56ca0164c70c305799563a76717f947860ea9
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-09-07 20:55:59 -0300

    menu.classList.add()

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,4 +2,6 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 
-btnMenu.addEventListener('click')
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add()
+})
