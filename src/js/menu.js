157d9ccee44f58f0f5715d552b69ba1e86365345
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-09-07 20:59:18 -0300

    .menu-mobile.abrir-menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,5 +3,5 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 
 btnMenu.addEventListener('click', ()=>{
-    menu.classList.add()
+    menu.classList.add('abrir-menu')
 })
